b65723f95722823228b93b04bf8f8d55c8fb980e
Karen <karenbanci1@gmail.com>
2025-05-05 19:42:14 -0700

    update base path in vite.config.js for game-react-three-fiber

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { transformWithEsbuild } from "vite";
 export default {
   root: "src/",
   publicDir: "../public/",
-  base: "./marble-race-3d-deploy/",
+  base: "./game-react-three-fiber/",
   plugins: [
     // React support
     react(),
